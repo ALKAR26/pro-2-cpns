c97673436ca69782fb35816d3ae9cac2a9355b4e
ALKAR26 <143257064+ALKAR26@users.noreply.github.com>
2023-09-17 12:52:30 +0530

    Update cpns.py

--- cpns.py
+++ cpns.py
@@ -3,49 +3,60 @@ from bs4 import BeautifulSoup
 import smtplib
 import time
 
-# Function to scrape car prices from a website
-def scrape_car_prices():
-    url = 'https://example.com/cars'  # Replace with the URL of the car listings page you want to scrape
-    headers = {'User-Agent': 'Your User Agent'}  # Replace with your user agent
+# Function to send an email notification
+def send_notification(subject, message, to_email):
+    from_email = "your_email@gmail.com"  # Replace with your email
+    password = "your_email_password"  # Replace with your email password
 
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Extract car prices from the website
-    car_prices = []
-    car_elements = soup.find_all('div', class_='car-listing')  # Adjust the class name accordingly
-
-    for car in car_elements:
-        price = car.find('span', class_='price').text  # Adjust the HTML structure
-        car_prices.append(price)
-
-    return car_prices
-
-# Function to send email notifications
-def send_email(subject, message):
-    sender_email = 'your_email@gmail.com'
-    sender_password = 'your_password'
-    receiver_email = 'receiver_email@gmail.com'
-
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+    # Create an SMTP server connection
+    server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
-    server.login(sender_email, sender_password)
+    server.login(from_email, password)
 
-    email_body = f'Subject: {subject}\n\n{message}'
-    server.sendmail(sender_email, receiver_email, email_body)
+    # Create the email message
+    email_message = f"Subject: {subject}\n\n{message}"
 
+    # Send the email
+    server.sendmail(from_email, to_email, email_message)
+
+    # Close the server connection
     server.quit()
 
-# Main loop to check car prices and send notifications
+# Function to scrape car prices
+def scrape_car_price(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Extract the price element based on the website's structure
+    price_element = soup.find("span", {"class": "price-class"})  # Replace with the actual HTML structure
+
+    if price_element:
+        return price_element.text.strip()
+    else:
+        return None
+
+# URL of the car listing page
+car_url = "https://example.com/car-listing"  # Replace with the URL of the car listing page
+
+# Desired price threshold
+desired_price = 20000
+
+# Email address to receive notifications
+to_email = "recipient_email@example.com"  # Replace with the recipient's email address
+
 while True:
-    car_prices = scrape_car_prices()
-
-    # Check if any car prices meet your criteria
-    for price in car_prices:
-        if int(price.replace('$', '').replace(',', '')) < 20000:  # Adjust your price threshold
-            subject = 'Car Price Alert!'
-            message = f'The price of a car is below $20,000: {price}'  # Customize the message
-            send_email(subject, message)
-
-    # Check again in 24 hours (adjust the time interval as needed)
+    current_price = scrape_car_price(car_url)
+    
+    if current_price:
+        current_price = float(current_price.replace("$", "").replace(",", ""))
+        
+        if current_price <= desired_price:
+            subject = "Car Price Alert"
+            message = f"The car price is now ${current_price}. It's within your desired price range!"
+            send_notification(subject, message, to_email)
+            break
+    
+    # Check the price every 24 hours (86400 seconds)
     time.sleep(86400)
