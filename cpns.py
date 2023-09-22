71c08bc5302a13a3d8670360d383d04fa361acec
ALKAR26 <143257064+ALKAR26@users.noreply.github.com>
2023-09-23 00:33:23 +0530

    Update cpns.py

--- cpns.py
+++ cpns.py
@@ -1,11 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox, QDialog, QInputDialog, QListWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QListWidget, QDialog, QInputDialog, QLineEdit, QPushButton, QMessageBox
 
 class CarPriceSystem(QWidget):
-    def __init__(self, cars):
+    def __init__(self, car_data):
         super().__init__()
 
-        self.cars = cars
+        self.car_data = car_data
         self.init_ui()
 
     def init_ui(self):
@@ -13,8 +13,10 @@ class CarPriceSystem(QWidget):
         self.setGeometry(100, 100, 400, 300)
 
         self.car_list = QListWidget(self)
-        for car, price in self.cars.items():
-            self.car_list.addItem(f"{car}: ${price:.2f}")
+        for city, companies in self.car_data.items():
+            for company, models in companies.items():
+                for model, price in models.items():
+                    self.car_list.addItem(f"{city} - {company} - {model}: ₹{price:.2f}")
 
         layout = QVBoxLayout()
         layout.addWidget(self.car_list)
@@ -22,10 +24,10 @@ class CarPriceSystem(QWidget):
         self.setLayout(layout)
 
 class LoginPage(QDialog):
-    def __init__(self, cars):
+    def __init__(self, car_data):
         super().__init__()
 
-        self.cars = cars
+        self.car_data = car_data
         self.init_ui()
         self.users = {}  # Dictionary to store user credentials
 
@@ -94,18 +96,78 @@ class LoginPage(QDialog):
                 QMessageBox.warning(self, 'Forgot Password', 'Username not found. Please check the username.')
 
 if __name__ == '__main__':
-    cars = {
-        'Car Model 1': 25000.00,
-        'Car Model 2': 32000.00,
-        'Car Model 3': 28000.00,
-        'Car Model 4': 35000.00,
+    car_data = {
+        'Delhi': {
+            'Toyota': {
+                'Camry': 3500000,
+                'Fortuner': 3500000,
+                'Corolla': 2200000,
+                'Innova': 2800000,
+                'Yaris': 1100000,
+            },
+            'Honda': {
+                'Civic': 2000000,
+                'CR-V': 3000000,
+                'Amaze': 700000,
+                'City': 1000000,
+                'BR-V': 1200000,
+            },
+        },
+        'Mumbai': {
+            'Maruti Suzuki': {
+                'Swift': 700000,
+                'Brezza': 900000,
+                'Alto': 400000,
+                'Wagon R': 500000,
+                'Ciaz': 900000,
+            },
+            'Hyundai': {
+                'i20': 800000,
+                'Creta': 1000000,
+                'Venue': 800000,
+                'Verna': 1000000,
+                'Tucson': 2500000,
+            },
+        },
+        'Chennai': {
+            'Mahindra': {
+                'XUV500': 1500000,
+                'Scorpio': 1200000,
+                'Thar': 1200000,
+                'Bolero': 800000,
+                'KUV100': 600000,
+            },
+            'Tata Motors': {
+                'Tiago': 550000,
+                'Tigor': 650000,
+                'Harrier': 1600000,
+                'Nexon': 800000,
+                'Safari': 2100000,
+            },
+        },
+        'Bangalore': {
+            'Kia': {
+                'Seltos': 1200000,
+                'Sonet': 800000,
+                'Carnival': 2500000,
+                'Niro': 1300000,
+                'Seltos EV': 1600000,
+            },
+            'Ford': {
+                'EcoSport': 900000,
+                'Figo': 700000,
+                'Endeavour': 3000000,
+                'Aspire': 800000,
+                'Freestyle': 800000,
+            },
+        },
     }
 
     app = QApplication(sys.argv)
 
-    login_page = LoginPage(cars)
+    login_page = LoginPage(car_data)
     if login_page.exec_() == QDialog.Accepted:
-        car_price_system = CarPriceSystem(cars)
+        car_price_system = CarPriceSystem(car_data)
         car_price_system.show()
 
     sys.exit(app.exec_())
