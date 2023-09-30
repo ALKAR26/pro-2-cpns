99abe0dab3853e6b51b829036ffb879455c515ad
Suraj Singh <143257064+ALKAR26@users.noreply.github.com>
2023-09-30 23:25:50 +0530

    Update cpns.py

--- cpns.py
+++ cpns.py
@@ -1,166 +1,85 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QListWidget, QDialog, QInputDialog, QLineEdit, QPushButton, QMessageBox, QListWidgetItem
-from PyQt5.QtCore import Qt  # Import Qt module from PyQt5
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QMessageBox
 
-class CarPriceSystem(QWidget):
-    def __init__(self, car_data):
+class UserTypeSelector(QMainWindow):
+    def __init__(self):
         super().__init__()
 
-        self.car_data = car_data
-        self.current_city = None  # Track the currently selected city
         self.init_ui()
 
     def init_ui(self):
-        self.setWindowTitle('Car Price System')
-        self.setGeometry(100, 100, 400, 300)
+        self.setWindowTitle("User Type Selector")
+        self.setGeometry(100, 100, 400, 200)
 
-        self.car_list = QListWidget(self)
-        self.update_car_list()  # Display car data for the initial city
+        self.label = QLabel(self)
+        self.label.setText("Select your user type:")
+        self.label.setGeometry(50, 50, 300, 30)
 
-        self.switch_city_button = QPushButton('Switch City', self)
-        self.switch_city_button.clicked.connect(self.switch_city)
+        self.admin_button = QPushButton("Admin", self)
+        self.admin_button.setGeometry(50, 100, 100, 40)
+        self.admin_button.clicked.connect(self.show_admin_login)
 
-        self.compare_button = QPushButton('Compare', self)
-        self.compare_button.setEnabled(False)  # Disable the Compare button initially
-        self.compare_button.clicked.connect(self.compare_cars)
+        self.user_button = QPushButton("User", self)
+        self.user_button.setGeometry(200, 100, 100, 40)
+        self.user_button.clicked.connect(self.user_selected)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.car_list)
-        layout.addWidget(self.switch_city_button)
-        layout.addWidget(self.compare_button)
+        self.admin_login_window = None
 
-        self.setLayout(layout)
+    def user_selected(self):
+        self.open_message_box("User")
 
-    def update_car_list(self):
-        # Clear the current car list
-        self.car_list.clear()
+    def open_message_box(self, user_type):
+        msg = f"You selected '{user_type}'"
+        self.label.setText(msg)
 
-        if self.current_city:
-            city_data = self.car_data.get(self.current_city, {})
-            for company, models in city_data.items():
-                for model, price in models.items():
-                    item = QListWidgetItem(f"{self.current_city} - {company} - {model}: ₹{price:.2f}", self.car_list)
-                    item.setFlags(item.flags() | Qt.ItemIsUserCheckable)
-                    item.setCheckState(Qt.Unchecked)
+    def show_admin_login(self):
+        self.admin_login_window = AdminLoginWindow()
+        self.admin_login_window.show()
 
-    def switch_city(self):
-        city, ok = QInputDialog.getItem(self, 'Switch City', 'Select a city:', list(self.car_data.keys()), 0, False)
-        if ok:
-            self.current_city = city
-            self.update_car_list()
-
-    def compare_cars(self):
-        selected_items = [item.text() for item in self.car_list.selectedItems()]
-
-        if selected_items:
-            QMessageBox.information(self, 'Compare Cars', '\n'.join(selected_items))
-        else:
-            QMessageBox.warning(self, 'Compare Cars', 'No cars selected for comparison. Please select some cars.')
-
-class LoginPage(QDialog):
-    def __init__(self, car_data):
+class AdminLoginWindow(QMainWindow):
+    def __init__(self):
         super().__init__()
 
-        self.car_data = car_data
         self.init_ui()
-        self.users = {}  # Dictionary to store user credentials
 
     def init_ui(self):
-        self.setWindowTitle('Login Page')
-        self.setGeometry(100, 100, 300, 200)
+        self.setWindowTitle("Admin Login")
+        self.setGeometry(100, 100, 400, 200)
 
-        self.username_label = QLabel('Username:', self)
-        self.username_entry = QLineEdit(self)
+        self.label = QLabel(self)
+        self.label.setText("Admin Login")
+        self.label.setGeometry(150, 20, 100, 30)
 
-        self.password_label = QLabel('Password:', self)
-        self.password_entry = QLineEdit(self)
-        self.password_entry.setEchoMode(QLineEdit.Password)
+        self.username_label = QLabel(self)
+        self.username_label.setText("Username:")
+        self.username_label.setGeometry(50, 70, 80, 30)
 
-        self.login_button = QPushButton('Login', self)
-        self.signup_button = QPushButton('Sign Up', self)
-        self.forgot_password_button = QPushButton('Forgot Password', self)
+        self.password_label = QLabel(self)
+        self.password_label.setText("Password:")
+        self.password_label.setGeometry(50, 120, 80, 30)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.username_label)
-        layout.addWidget(self.username_entry)
-        layout.addWidget(self.password_label)
-        layout.addWidget(self.password_entry)
-        layout.addWidget(self.login_button)
-        layout.addWidget(self.signup_button)
-        layout.addWidget(self.forgot_password_button)
+        self.username_input = QLineEdit(self)
+        self.username_input.setGeometry(150, 70, 200, 30)
 
-        self.setLayout(layout)
+        self.password_input = QLineEdit(self)
+        self.password_input.setGeometry(150, 120, 200, 30)
+        self.password_input.setEchoMode(QLineEdit.Password)
 
-        self.login_button.clicked.connect(self.login)
-        self.signup_button.clicked.connect(self.signup)
-        self.forgot_password_button.clicked.connect(self.forgot_password)
+        self.login_button = QPushButton("Login", self)
+        self.login_button.setGeometry(150, 170, 100, 30)
+        self.login_button.clicked.connect(self.admin_login)
 
-    def login(self):
-        username = self.username_entry.text()
-        password = self.password_entry.text()
+    def admin_login(self):
+        username = self.username_input.text()
+        password = self.password_input.text()
 
-        if username in self.users and self.users[username] == password:
-            self.accept()  # Close the login dialog and open the main application window
+        if username == "ALKAR" and password == "12345678":
+            QMessageBox.information(self, "Admin Login", "Login Successful")
         else:
-            QMessageBox.critical(self, 'Login', 'Login Failed. Invalid username or password.')
-
-    def signup(self):
-        username = self.username_entry.text()
-        password = self.password_entry.text()
-
-        if not username or not password:
-            QMessageBox.warning(self, 'Sign Up', 'Username and Password are required for sign up.')
-            return
-
-        if username in self.users:
-            QMessageBox.warning(self, 'Sign Up', 'Username already exists. Please choose another.')
-        else:
-            self.users[username] = password
-            QMessageBox.information(self, 'Sign Up', 'Sign Up Successful!')
-
-    def forgot_password(self):
-        username, ok = QInputDialog.getText(self, 'Forgot Password', 'Enter your username:')
-        if ok:
-            if username in self.users:
-                new_password, ok = QInputDialog.getText(self, 'Forgot Password', 'Enter a new password:')
-                if ok:
-                    self.users[username] = new_password
-                    QMessageBox.information(self, 'Forgot Password', 'Password reset successful!')
+            QMessageBox.warning(self, "Admin Login", "Invalid Username or Password")
 
 if __name__ == '__main__':
-    # Define a common set of car models for all cities
-    common_car_models = {
-        'Toyota': {
-            'Camry': 3500000,
-            'Fortuner': 3500000,
-            'Corolla': 2200000,
-            'Innova': 2800000,
-            'Yaris': 1100000,
-        },
-        'Honda': {
-            'Civic': 2000000,
-            'CR-V': 3000000,
-            'Amaze': 700000,
-            'City': 1000000,
-            'BR-V': 1200000,
-        },
-    }
-
-    # Create car data for different cities
-    car_data = {
-        'Delhi': common_car_models,
-        'Mumbai': common_car_models,
-        'Chennai': common_car_models,
-        'Bangalore': common_car_models,
-    }
-
     app = QApplication(sys.argv)
-
-    login_page = LoginPage(car_data)
-    if login_page.exec_() == QDialog.Accepted:
-        car_price_system = CarPriceSystem(car_data)
-        car_price_system.show()
-
+    window = UserTypeSelector()
+    window.show()
     sys.exit(app.exec_())
-
-    
