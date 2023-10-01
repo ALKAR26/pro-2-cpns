46fce7cb30bade4189e6c012d475e2aed117bc74
Suraj Singh <143257064+ALKAR26@users.noreply.github.com>
2023-10-01 13:52:06 +0530

    Update cpns.py

--- cpns.py
+++ cpns.py
@@ -1,5 +1,48 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget, QLineEdit, QDialog, QMessageBox
+
+class AdminLoginDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Admin Login")
+        self.setGeometry(100, 100, 400, 200)
+
+        self.layout = QVBoxLayout()
+
+        self.label = QLabel("Admin Login")
+        self.layout.addWidget(self.label)
+
+        self.username_label = QLabel("Username:")
+        self.layout.addWidget(self.username_label)
+
+        self.username_input = QLineEdit()
+        self.layout.addWidget(self.username_input)
+
+        self.password_label = QLabel("Password:")
+        self.layout.addWidget(self.password_label)
+
+        self.password_input = QLineEdit()
+        self.password_input.setEchoMode(QLineEdit.Password)
+        self.layout.addWidget(self.password_input)
+
+        self.login_button = QPushButton("Login")
+        self.login_button.clicked.connect(self.login)
+        self.layout.addWidget(self.login_button)
+
+        self.setLayout(self.layout)
+
+    def login(self):
+        username = self.username_input.text()
+        password = self.password_input.text()
+
+        if username == "ALKAR" and password == "12345678":
+            # Successful login
+            QMessageBox.information(self, "Success", "Admin login successful.")
+            self.accept()
+        else:
+            # Failed login
+            QMessageBox.warning(self, "Error", "Invalid username or password.")
 
 class UserSelectionWindow(QMainWindow):
     def __init__(self):
@@ -17,7 +60,7 @@ class UserSelectionWindow(QMainWindow):
         self.layout.addWidget(self.label)
 
         self.admin_button = QPushButton("Admin User")
-        self.admin_button.clicked.connect(self.admin_selected)
+        self.admin_button.clicked.connect(self.show_admin_login)
         self.layout.addWidget(self.admin_button)
 
         self.user_button = QPushButton("Simple User")
@@ -26,9 +69,10 @@ class UserSelectionWindow(QMainWindow):
 
         self.central_widget.setLayout(self.layout)
 
-    def admin_selected(self):
-        # Handle admin user selection
-        print("Admin User selected")
+    def show_admin_login(self):
+        admin_login_dialog = AdminLoginDialog()
+        if admin_login_dialog.exec_() == QDialog.Accepted:
+            print("Admin login successful")
 
     def user_selected(self):
         # Handle simple user selection
