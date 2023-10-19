312e4e1a7f40f27ab4c507592e943920f847311c
Suraj Singh <143257064+ALKAR26@users.noreply.github.com>
2023-10-20 01:03:53 +0530

    Update cpns.py

--- cpns.py
+++ cpns.py
@@ -1,14 +1,53 @@
-from plyer import notification
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox
 
-def send_notification(title, message):
-    notification.notify(
-        title=title,
-        message=message,
-        app_name="YourApp"  # Change this to your app's name
-    )
+class CarPriceNotifier(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-if __name__ == "__main__":
-    notification_title = "Notification Title"
-    notification_message = "This is a pop-up notification."
+    def initUI(self):
+        self.setWindowTitle('Car Price Notifier')
+        self.setGeometry(100, 100, 400, 200)
 
-    send_notification(notification_title, notification_message)
+        layout = QVBoxLayout()
+
+        self.car_name_label = QLabel('Car Name:')
+        self.car_name_input = QLineEdit()
+        layout.addWidget(self.car_name_label)
+        layout.addWidget(self.car_name_input)
+
+        self.car_price_label = QLabel('Car Price:')
+        self.car_price_input = QLineEdit()
+        layout.addWidget(self.car_price_label)
+        layout.addWidget(self.car_price_input)
+
+        self.threshold_label = QLabel('Price Threshold:')
+        self.threshold_input = QLineEdit()
+        layout.addWidget(self.threshold_label)
+        layout.addWidget(self.threshold_input)
+
+        self.track_button = QPushButton('Track Price')
+        self.track_button.clicked.connect(self.track_price)
+        layout.addWidget(self.track_button)
+
+        self.setLayout(layout)
+
+    def track_price(self):
+        car_name = self.car_name_input.text()
+        car_price = float(self.car_price_input.text())
+        threshold = float(self.threshold_input.text())
+
+        if car_price < threshold:
+            QMessageBox.information(self, 'Price Alert', f'The price of {car_name} is below your threshold!')
+        else:
+            QMessageBox.information(self, 'Price Alert', f'The price of {car_name} is not below your threshold.')
+
+def main():
+    app = QApplication(sys.argv)
+    window = CarPriceNotifier()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
